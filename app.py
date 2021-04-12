cf53a69fa982ab84abd034baee823bfdd12f628a
yinganmin <yinganmin6837@163.com>
2021-04-12 21:48:31 +0800

    add poi.py and modify homepage

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, busline, gaodekey
+from apps import home, busline, poi, gaodekey
 
 app = MultiApp()
 
@@ -12,6 +12,7 @@ st.markdown("""
 # Add all your application here
 app.add_app("Home", home.app)
 app.add_app("城市公交线路可视化及下载系统", busline.app)
+app.add_app("高德POI抓取系统", poi.app)
 app.add_app("高德APP Key", gaodekey.app)
 
 # The main app
--- apps/home.py
+++ apps/home.py
@@ -4,5 +4,21 @@ import streamlit as st
 def app():
     st.title('Home')
 
-    st.write('This is the `home page` of this multi-page app.')
-    st.write('In this app, we will be building a simple classification model using the Iris dataset.')
+    st.subheader("Introduction")
+    st.markdown("We are the **Spatio-Temporal Social Sensing Lab of Peking University ($S^3$-Lab).**")
+    st.markdown("We are developing this tool platform based on [Python](https://python.org) and [Streamlit](https://streamlit.io), \
+        the purpose is to facilitate GISers to quickly grab data, obtain information and finally complete the work.")
+    
+    st.subheader("Function")
+    st.markdown("Currently, the existing functions of the platform are as follows:")
+    st.markdown("- Visualization and download system of urban bus lines")
+    st.markdown("- Amap POI Crawling System")
+    st.markdown("- Amap/Bmap API Key")
+    st.markdown("**If you have any questions or ideas, feel free to contact us!**")
+    
+    st.subheader("Contributors")
+    st.markdown("@[CUGbaoyi](https://github.com/CUGbaoyi), @[sunshineYin](https://github.com/sunshineYin), @[HanwGeek](https://github.com/HanwGeek)")
+
+    st.subheader("Affiliation")
+    # st.write("<img src='https://i.loli.net/2021/04/12/qxTjodCSg72KksW.jpg' height='50%' width='50%'/>")
+    st.image('https://i.loli.net/2021/04/12/qxTjodCSg72KksW.jpg')
